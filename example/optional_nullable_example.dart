d72b7974fe471c692eef1ba2413d7b3d93789b39
Robert Tyssen <rob.tyssen@gmail.com>
2022-05-15 19:06:52 -0400

    Initial commit

---
+++ example/optional_nullable_example.dart
@@ -0,0 +1,6 @@
+import 'package:optional_nullable/optional_nullable.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/optional_nullable.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library optional_nullable;
+
+export 'src/optional_nullable_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
